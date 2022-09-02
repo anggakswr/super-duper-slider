cc4374e74c1093d8bc2402973fc8e3a41fa8b946
Kamiswara Angga W <kamiswara98@gmail.com>
2022-09-02 15:59:24 +0700

    Added nav dots for slider2

--- components/Slider2.vue
+++ components/Slider2.vue
@@ -1,30 +1,47 @@
 <template>
-  <!-- slider container container (to add nav btns) -->
-  <div class="flex items-center gap-x-8">
-    <!-- prev btn -->
-    <button class="text-white text-2xl" @click="prev">&larr;</button>
+  <!-- slider container container container (to add dot nav btns below) -->
+  <div>
+    <!-- slider container container (to add nav btns) -->
+    <div class="flex items-center gap-x-8">
+      <!-- prev btn -->
+      <button class="text-white text-2xl" @click="prev">&larr;</button>
 
-    <!-- slider container -->
-    <div class="w-[500px] overflow-hidden">
-      <!-- the slide items will collapse horizontally -->
-      <div class="flex items-center">
-        <!-- img container -->
-        <div
-          v-for="number in 5"
-          :key="'pic-' + number"
-          class="w-[500px] min-w-[500px] max-w-[500px]"
-          :style="{ transform: `translateX(${-500 * (currentSlide - 1)}px)` }"
-        >
-          <img
-            :src="`/slider2/${number}.jpg`"
-            :alt="'Slider 2 Pic ' + number"
-          />
+      <!-- slider container -->
+      <div class="w-[500px] overflow-hidden">
+        <!-- the slide items will collapse horizontally -->
+        <div class="flex items-center">
+          <!-- img container -->
+          <div
+            v-for="number in 5"
+            :key="'pic-' + number"
+            class="w-[500px] min-w-[500px] max-w-[500px]"
+            :style="{ transform: `translateX(${-500 * (currentSlide - 1)}px)` }"
+          >
+            <img
+              :src="`/slider2/${number}.jpg`"
+              :alt="'Slider 2 Pic ' + number"
+            />
+          </div>
         </div>
       </div>
+
+      <!-- next btn -->
+      <button class="text-white text-2xl" @click="next">&rarr;</button>
     </div>
 
-    <!-- next btn -->
-    <button class="text-white text-2xl" @click="next">&rarr;</button>
+    <!-- dots -->
+    <div class="flex items-center justify-center gap-x-4 py-4">
+      <button
+        v-for="number in 5"
+        :key="'slider2-dot-' + number"
+        class="h-4 rounded-full"
+        :class="{
+          'bg-white w-4': currentSlide !== number,
+          'bg-blue-500 w-12': currentSlide === number,
+        }"
+        @click="currentSlide = number"
+      />
+    </div>
   </div>
 </template>
 
